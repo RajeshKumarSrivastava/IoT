c86dfe31e2660d1350db61e464ee2ba5432a6fd7
Rajesh Kumar Srivastava <rajeshk.life@gmail.com>
2017-10-29 09:40:43 +0530

    Intial

---
+++ ObsatcleCarWithLeds/Debug/ObsatcleCarWithLeds.cpp.d
@@ -0,0 +1,18 @@
+C:\Users\rs250641\AppData\Local\Temp\VMBuilds\ObsatcleCarWithLeds\uno\Debug\ObsatcleCarWithLeds.cpp.o: \
+ C:\Users\rs250641\AppData\Local\Temp\VMBuilds\ObsatcleCarWithLeds\uno\Debug\ObsatcleCarWithLeds.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\rs250641\Documents\Arduino\libraries\Adafruit-Motor-Shield-library/AFMotor.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/Servo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/avr/ServoTimers.h \
+ C:\Users\rs250641\Documents\Arduino\libraries\NewPing/NewPing.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\xitkcou5.0co\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
